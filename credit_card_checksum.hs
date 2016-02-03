9265dfdfedd6a2ee716148854fc28b3907f3b273
Abe Schonfeld <aschonfeld@groupon.com>
2016-02-03 12:34:40 -0500

    refactors

--- credit_card_checksum.hs
+++ credit_card_checksum.hs
@@ -1,14 +1,22 @@
+mod10 = flip mod 10
+div10 = flip div 10
+
 intToDigits :: Int -> [Int]
-intToDigits x =
-  if x `div` 10 == 0 then [x] else  intToDigits (div x 10) ++ [mod x 10]
+intToDigits x
+  | (xDiv10 == 0) = [x]
+  | otherwise = intToDigits (xDiv10) ++ [mod10 x]
+  where xDiv10 = div10 x
 
 doubleFromRight :: [Int] -> [Int]
 doubleFromRight list =
   fst $ foldr (\x (acc, bool) ->
     ((if bool then 2 * x else x) : acc, not bool)) ([], False) list
 
+
 sumDigits :: [Int] -> Int
-sumDigits list = foldr (+) 0 (concat (map intToDigits list))
+sumDigits list = foldr (+) 0 $ concat $ map intToDigits list
 
 validate :: Int -> Bool
-validate num = sumDigits (doubleFromRight (intToDigits num)) `mod` 10 == 0
+validate num = mod10 (sumDigits (doubleFromRight  $ intToDigits num)) == 0
+
+-- validate
